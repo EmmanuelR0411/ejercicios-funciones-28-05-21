130f668baa0e727cb45726b9af1bbc3b4749048f
Emmanuel <emmanuelreyna0410@gmail.com>
2021-06-19 11:38:40 -0500

    Aprendiendo funciones js

---
+++ ciclos_arreglo.js
@@ -0,0 +1,6 @@
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo',
+            'Junio','Julio','Agosto', 'Septiembre','Octubre','Noviembre','Diciembre']
+
+for(var i = 0; i < 12; i++){
+    console.log('Mes numero', m=i+1, 'del año:',meses [i] );
+}
---
+++ suma_funcion.js
@@ -0,0 +1,12 @@
+function suma (numero1,numero2) {
+
+    var resultado = numero1 + numero2;
+    console.log('Suma de: ', numero1, '+', numero2)
+    console.log('Resultado: ' + resultado)
+}
+
+suma(5,2);
+suma(1789,432.20);
+suma(-600, 1872);
+suma(.000098, 1.3777771);
+suma(0,69);
